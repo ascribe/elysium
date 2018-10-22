5a3cfb1e08f017b3b1c31d6ae8d9a08cf081c47e
Matthias Kretschmann <m@kretschmann.io>
2018-10-22 14:29:51 +0200

    add deployment script

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+aws s3 sync ./ s3://www.ascribe.io \
+    --delete \
+    --acl public-read \
+    --exclude '.DS_Store' \
+    --exclude '.editorconfig' \
+    --exclude 'README.md' \
+    --exclude 'LICENSE' \
+    --cache-control max-age=0,no-cache,no-store,must-revalidate
