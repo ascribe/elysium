35f0f4bb60a75c6801d66a77da7aae4834145ae9
Troy McConaghy <troy@bigchaindb.com>
2018-10-23 15:11:01 +0200

    Exclude deploy.sh when uploading to S3

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,5 @@ aws s3 sync ./ s3://www.ascribe.io \
     --exclude '.editorconfig' \
     --exclude 'README.md' \
     --exclude 'LICENSE' \
+    --exclude 'deploy.sh' \
     --cache-control max-age=0,no-cache,no-store,must-revalidate
